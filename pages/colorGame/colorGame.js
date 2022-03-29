22a13c321b946c69bbc990346d3c8c7772ef18af
yifengjianbaiwl <617306046@qq.com>
2022-03-29 16:02:26 +0800

    添加倒计时进度条，得分

--- pages/colorGame/colorGame.js
+++ pages/colorGame/colorGame.js
@@ -1,14 +1,19 @@
 Page({
   data: {
     rowData:[
-      {column:['#AA8877','#AA8877','#AA8877','#AA8877']},
-      {column:['#AA8877','#AA8877','#AA8877','#AA8877']},
-      {column:['#AA8877','#AA8877','#AA8877','#AA8877']},
-      {column:['#AA8877','#AA8877','#BB5588','#AA8877']},
-      {column:['#AA8877','#AA8877','#AA8877','#AA8877']},
+      {column:['#4a86e8','#4a86e8','#4a86e8','#4a86e8']},
+      {column:['#4a86e8','#4a86e8','#4a86e8','#4a86e8']},
+      {column:['#4a86e8','#4a86e8','#4a86e8','#4a86e8']},
+      {column:['#4a86e8','#4a86e8','#1160e1','#4a86e8']},
+      {column:['#4a86e8','#4a86e8','#4a86e8','#4a86e8']},
     ],
     round:1,
-    spColor:"#BB5588"
+    spColor:"#1160e1",
+    second:5,
+    percent:100,
+    intervId:0,
+    flag:false,
+    score:0,
   },
   onLoad() {
     
@@ -16,32 +21,28 @@ Page({
   init(){
     let thisData = this.data;
     thisData.rowData = [
-      {column:['#AA8877','#AA8877','#AA8877','#AA8877']},
-      {column:['#AA8877','#AA8877','#AA8877','#AA8877']},
-      {column:['#AA8877','#AA8877','#AA8877','#AA8877']},
-      {column:['#AA8877','#AA8877','#BB5588','#AA8877']},
-      {column:['#AA8877','#AA8877','#AA8877','#AA8877']},
+      {column:['#4a86e8','#4a86e8','#4a86e8','#4a86e8']},
+      {column:['#4a86e8','#4a86e8','#4a86e8','#4a86e8']},
+      {column:['#4a86e8','#4a86e8','#4a86e8','#4a86e8']},
+      {column:['#4a86e8','#4a86e8','#1160e1','#4a86e8']},
+      {column:['#4a86e8','#4a86e8','#4a86e8','#4a86e8']},
     ]
-    this.setData({
-      rowData:thisData.rowData
-    })
-  },
-  tapBlock2(){
-    let thisData = this.data;
-    //循环给每个block添加颜色
-    for(let i=0; i<thisData.rowData.length;i++){
-      let r = thisData.rowData[i];
-      for(let j=0; j< r.column.length;j++){
-        r.column[j]="#ff0000";
-      }
-    }
     this.setData({
       rowData:thisData.rowData
     })
   },
   tapBlock(data) {
     let color = data.currentTarget.dataset.color;
-    let thisData = this.data;
+    let that = this;
+    let thisData = that.data;
+    that.setData({
+      score:thisData.score+thisData.second
+    })
+    that.setData({
+      second:5,
+      flag:true,
+      percent:thisData.percent==100?99:100,
+    })
     if(color==thisData.spColor){
       let bgR = Math.floor(Math.random()*255+1);//红色通道
       let bgG = Math.floor(Math.random()*255+1);//绿色通道
@@ -59,8 +60,8 @@ Page({
           //生成不一样的颜色
           if(temIndex==spIndex){
             let spBgR = this.offSetColor(thisData.round, bgR)
-            let spBgG = this.offSetColor(thisData.round, bgG)
             let spBgB = this.offSetColor(thisData.round, bgB)
+            let spBgG = this.offSetColor(thisData.round, bgG)
             spBgColor = "#" + this.formatString(spBgR.toString(16)) + this.formatString(spBgG.toString(16)) + this.formatString(spBgB.toString(16));
             r.column[j]=spBgColor;
           }
@@ -72,30 +73,52 @@ Page({
         spColor:spBgColor,
         round:thisData.round
       })
+      clearInterval(thisData.intervId);
+      thisData.intervId = setInterval(function(){
+        thisData.second-=1;
+        that.setData({
+          second:thisData.second,
+        })
+      },1000);
       if(thisData.round>100){
         wx.showModal({
           title:'提示',
           content:'恭喜您通过',
           cancelColor: "#DD8800",
         })
+        this.setData({
+          spColor:"#1160e1",
+          round:1,
+          flag:false,
+          second:5
+        })
         this.init();
       }
     }else{
-      wx.showModal({
-        title:'提示',
-        content:'挑战失败，当前关数第'+thisData.round+'关',
-        cancelColor: "#DD8800",
-      })
       this.setData({
-        spColor:"#BB5588",
-        round:1
+        flag:false
       })
-      this.init();
     }
+  },
+  endTime(){
+    clearInterval(this.data.intervId);
+    wx.showModal({
+      title:'提示',
+      content:'挑战失败，当前关数第'+this.data.round+'关，当前得分：' + this.data.score,
+      cancelColor: "#DD8800",
+    })
+    this.setData({
+      spColor:"#1160e1",
+      round:1,
+      second:5,
+      flag:false,
+      score:0
+    })
+    this.init();
   },
   //根据关卡数量和正常颜色生成不一样的颜色
   offSetColor(round, color){
-    let offsetindex = 100-round;
+    let offsetindex = 50-round*2;
     if(offsetindex<=0){
       offsetindex = 1;
     }
